99617b549e9825646b6e18d8c8c14bee99c000f4
John <john@primitive.io>
2023-01-26 16:58:46 -0800

    add image support

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.CommandLine;
+using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
 using System.Drawing;
 using GleamTech.VideoUltimate;
@@ -11,7 +11,7 @@ public class Program
         public string OutputFolderPath { get; set; }
 
         public double TimeStampInSeconds { get; set; }
-        
+
         public string NerfExePath { get; set; }
     }
 
@@ -24,14 +24,14 @@ public class Program
             new Argument<string>("OutputFolderPath"),
 
             new Option<double>("--timeStampInSeconds", "The frame number to extract"),
-            
+
             new Option<string>("--nerfExePath", "The path to the nerf executable")
         };
 
-        rootCommand.Description = "Video to image converter";
+        rootCommand.Description = "Video to NeRF converter, or image to NeRF converter";
 
         // Note that the parameters of the handler method are matched according to the names of the options 
-        rootCommand.Handler = CommandHandler.Create<Args>(NerfVideos);
+        rootCommand.Handler = CommandHandler.Create<Args>(NerfMedia);
 
         rootCommand.Invoke(args);
 
@@ -42,38 +42,65 @@ public class Program
     /// Iterate through all .mp4 files in the input folder and extract the frame number from each video and write each frame to the output folder
     /// </summary>
     /// <param name="args"></param>
-    static void NerfVideos(Args args)
+    static void NerfMedia(Args args)
     {
         string inputPath = args.InputPath;
         string outputFolderPath = args.OutputFolderPath;
 
-        double timeStampInSeconds = args.TimeStampInSeconds;
+        string imagesPath;
 
-        string frameFolderName = RoundDoubleToInt(timeStampInSeconds).ToString();
-        string outputDirectoryFramePath = Path.Combine(outputFolderPath, frameFolderName);
-        if (Directory.Exists(outputDirectoryFramePath))
+        if (args.TimeStampInSeconds <= double.Epsilon)
         {
-            Directory.Delete(outputDirectoryFramePath, true);
+            double timeStampInSeconds = args.TimeStampInSeconds;
+
+            imagesPath = RoundDoubleToInt(timeStampInSeconds).ToString();
+            string outputDirectoryFramePath = Path.Combine(outputFolderPath, imagesPath);
+            if (Directory.Exists(outputDirectoryFramePath))
+            {
+                Directory.Delete(outputDirectoryFramePath, true);
+            }
+
+            Directory.CreateDirectory(outputDirectoryFramePath);
+
+            // iterate through all .mp4 files in the input folder
+            foreach (string filePath in Directory.GetFiles(inputPath, "*.mp4"))
+            {
+                // extract the frame number from each video and write each frame to the output folder
+                ExtractFrame(filePath, outputFolderPath, timeStampInSeconds);
+            }
+        }
+        else
+        {
+            imagesPath = Path.Combine(outputFolderPath, "images");
+            if (Directory.Exists(imagesPath))
+            {
+                Directory.Delete(imagesPath, true);
+            }
+
+            Directory.CreateDirectory(imagesPath);
+
+            foreach (string filePath in Directory.GetFiles(inputPath, "*.jpg"))
+            {
+                string fileName = Path.Combine(imagesPath, Path.GetFileName(filePath));
+                File.Copy(filePath, fileName);
+            }
+
+            foreach (string filePath in Directory.GetFiles(inputPath, "*.png"))
+            {
+                string fileName = Path.Combine(imagesPath, Path.GetFileName(filePath));
+                File.Copy(filePath, fileName);
+            }
         }
 
-        Directory.CreateDirectory(outputDirectoryFramePath);
-
-        // iterate through all .mp4 files in the input folder
-        foreach (string filePath in Directory.GetFiles(inputPath, "*.mp4"))
-        {
-            // extract the frame number from each video and write each frame to the output folder
-            ExtractFrame(filePath, outputFolderPath, timeStampInSeconds);
-        }
-
-        ColmapRunner colmapRunner = new ColmapRunner(args.OutputFolderPath, frameFolderName);
+        ColmapRunner colmapRunner = new ColmapRunner(args.OutputFolderPath, imagesPath);
 
         colmapRunner.RunColmapAutomatic();
         //colmapRunner.Efficient();
         colmapRunner.MapAndConvert();
 
         Colmap2Nerf colmap2Nerf = new Colmap2Nerf(outputFolderPath);
-        colmap2Nerf.Convert(frameFolderName);
-        
+        colmap2Nerf.Convert(imagesPath);
+
         NerfRunner nerfRunner = new NerfRunner(args.NerfExePath, outputFolderPath);
         nerfRunner.RunNerf();
     }
